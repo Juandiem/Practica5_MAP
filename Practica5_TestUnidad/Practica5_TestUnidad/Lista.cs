9dba2d66d2b0060faf7da85d0a521385ace75192
Oxelion <juandiem@ucm.es>
2022-04-01 16:23:17 +0200

    Clase lista actualizada

--- Practica5_TestUnidad/Practica5_TestUnidad/Lista.cs
+++ Practica5_TestUnidad/Practica5_TestUnidad/Lista.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Listas {
 	/// <summary>
@@ -7,11 +8,19 @@ namespace Listas {
 	/// </summary>
 	public class Lista {
 
+		List<int> list;
+		
 		/// <summary>
         /// Constructora de la lista. 
         /// Construye una lista sin elementos
         /// </summary>
-		public Lista() {
+		public Lista(int limite, int rep) {
+			int n = 1;
+			for (int i = 0; i < limite * rep; i++)
+            {
+				list.Add(n);
+				n = (limite % n) + 1;
+            }
 		}
 
 		/// <summary>
@@ -19,7 +28,7 @@ namespace Listas {
 		/// </summary>
 		/// <returns>Devuelve el número de elementos (0 si es vacía)</returns>
 		public int NumElems() {
-			return 0;
+			return list.Count;
 		}
 
 		/// <summary>
@@ -30,7 +39,14 @@ namespace Listas {
 		/// <exception cref="System.Exception">Lanza una excepción en caso de que no exista la posición</exception>
 		public int NEsimo(int n)
 		{
-			return 0;
+			try
+			{
+				return list[n];
+			}
+            catch (IndexOutOfRangeException ex)
+            {
+				throw new ArgumentException("Index is out of range", nameof(n), ex);
+			}
 		}
 
 		/// <summary>
@@ -40,7 +56,10 @@ namespace Listas {
 		/// <returns>True si el elemento está en la lista; false en otro caso</returns>
 		public bool Esta(int e)
 		{
-			return false;
+			int i = 0;
+			while (i < list.Count && list[i] != e) i++;
+			if (i < list.Count) return true;
+			else return true;
 		}
 
 		/// <summary>
@@ -48,6 +67,7 @@ namespace Listas {
 		/// </summary>
 		/// <param name="x">Elemento a insertar en la lista</param>
 		public void InsertaFin(int x) {
+			list.Add(x);
 		}
 
 		/// <summary>
@@ -55,6 +75,7 @@ namespace Listas {
 		/// </summary>
 		/// <param name="x">Elemento a eliminar</param>
 		public void EliminaElem(int x) {
+			list.RemoveAt(x);
 		}
 	}
 }
